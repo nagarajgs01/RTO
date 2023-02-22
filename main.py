89acb08ad4650a4c271ac4e2726cd7e6f1301ce0
Nagaraj G Sheeli <76840609+nagarajgs01@users.noreply.github.com>
2023-02-22 23:28:21 +0530

    Updated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-# import openpyxl
+import openpyxl
 from datetime import date
 from datetime import datetime
 from pathlib import Path
@@ -7,20 +7,25 @@ from docxtpl import DocxTemplate
 
 base_dir = Path(__file__).parent
 word = "F:/7th Sem/RTO Project/Book1.docx"
-excel = "F:/7th Sem/RTO Project/Book.xlsx"
+excel = "F:/7th Sem/RTO Project/Book.xlsx"  # file
 output = base_dir / "OUTPUT"
 
 output.mkdir(exist_ok=True)
 
 fd = pd.read_excel(excel, 'Sheet1')
 date = date.today()
+d = date.strftime("%d-%m-%y")
+fd.insert(4, column="date", value=d)
+fd.insert(0, column="Sno", value=range(1, len(fd) + 1))
+print(fd.head())
+print(fd.to_dict("records"))
 
 for record in fd.to_dict("records"):
-    doc = DocxTemplate(word)
-    doc.render(record)
-    output_path = output / f"{record['Name']}-doc.docx"
-    doc.save(output_path)
-    # print(record['Done'])
+    if 2 <= record['Sno'] <= 4:
+        doc = DocxTemplate(word)
+        doc.render(record)
+        output_path = output / f"{record['Name']}-doc.docx"
+        doc.save(output_path)
 
 # if record['Done'].equals("True"):
 
@@ -34,3 +39,4 @@ for record in fd.to_dict("records"):
 #     count += 1
 #
 # pd.
+
