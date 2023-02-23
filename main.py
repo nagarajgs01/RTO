554e6228425d5ef6aa959ee83d645b61ed2cb501
Nagaraj G Sheeli <76840609+nagarajgs01@users.noreply.github.com>
2023-02-23 12:27:35 +0530

    Range ,single and all Docx Code Updated

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from pathlib import Path
 from docxtpl import DocxTemplate
 
 base_dir = Path(__file__).parent
-word = "F:/7th Sem/RTO Project/Book1.docx"
-excel = "F:/7th Sem/RTO Project/Book.xlsx"  # file
+word = base_dir / "Book1.docx"
+excel = base_dir / "Book.xlsx"  # file
 output = base_dir / "OUTPUT"
 
 output.mkdir(exist_ok=True)
@@ -20,12 +20,50 @@ fd.insert(0, column="Sno", value=range(1, len(fd) + 1))
 print(fd.head())
 print(fd.to_dict("records"))
 
+print("1 ) If Want to print single value \n2 ) If want to print a range \n3 ) Print All")
+ch=input("Enter your choice")
+print(ch)
+
+if(ch=='1'):
+   sno=int(input("Enter the sno of the member"))   
+   for record in fd.to_dict("records"):       
+       if record['Sno'] == sno:
+           doc = DocxTemplate(word)
+           doc.render(record)
+           output_path = output / f"{record['Name']}-doc.docx"
+           doc.save(output_path)
+    print("Printed document with sno")
+    
+elif(ch=='2'):
+    range=input("Enter the Range seperated by - :ex 1-4 ")
+    lst=range.split('-')
+    print(lst)
+    a=int(lst[0])
+    b=int(lst[1])
+    for record in fd.to_dict("records"):
+        if a <= record['Sno'] <= b:
+            doc = DocxTemplate(word)
+            doc.render(record)
+            output_path = output / f"{record['Name']}-doc.docx"
+            doc.save(output_path)
+    print("Printed docs Succesfully")
+else:
+    for record in fd.to_dict("records"):
+        doc = DocxTemplate(word)
+        doc.render(record)
+        output_path = output / f"{record['Name']}-doc.docx"
+        doc.save(output_path)
+    print("Printed all the docs Successfully")
+        
+
+'''
 for record in fd.to_dict("records"):
     if 2 <= record['Sno'] <= 4:
         doc = DocxTemplate(word)
         doc.render(record)
         output_path = output / f"{record['Name']}-doc.docx"
         doc.save(output_path)
+    '''
 
 # if record['Done'].equals("True"):
 
