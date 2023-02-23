532433b581ca419cd2ac639599b1cfd8f35f1479
Nagaraj G Sheeli <76840609+nagarajgs01@users.noreply.github.com>
2023-02-23 17:48:45 +0530

    Full updated

--- main.py
+++ main.py
@@ -5,12 +5,53 @@ from datetime import datetime
 from pathlib import Path
 from docxtpl import DocxTemplate
 
+
+def comma(a):
+    str = []
+    for i in a:
+        if i.__contains__("-"):
+            hifen(i)
+        else:
+            for record in fd.to_dict("records"):
+                c = int(i)
+                if record['Sno'] == c:
+                    str.append(c)
+                    doc = DocxTemplate(word)
+                    doc.render(record)
+                    output_path = output / f"{record['Name']}-doc.docx"
+                    doc.save(output_path)
+    if len(str) > 0:
+        print(f"Successfully printed data with sno : {str}")
+
+
+def hifen(b):
+    lst = b.split('-')
+    print(lst)
+    a = int(lst[0])
+    b = int(lst[1])
+    for record in fd.to_dict("records"):
+        if a <= record['Sno'] <= b:
+            doc = DocxTemplate(word)
+            doc.render(record)
+            output_path = output / f"{record['Name']}-doc.docx"
+            doc.save(output_path)
+    print(f"Successfully printed from {a} to {b}")
+
+
 base_dir = Path(__file__).parent
-word = base_dir / "Book1.docx"
-excel = base_dir / "Book.xlsx"  # file
+word = "F:/7th Sem/RTO Project/Book1.docx"
+excel = "F:/7th Sem/RTO Project/Book.xlsx"  # file
 output = base_dir / "OUTPUT"
 
 output.mkdir(exist_ok=True)
+
+# fd = pd.read_excel(excel, 'Sheet1')
+# date = date.today()
+# d = date.strftime("%d-%m-%y")
+# fd.insert(4, column="date", value=d)
+# fd.insert(0, column="Sno", value=range(1, len(fd) + 1))
+# print(fd.head())
+# print(fd.to_dict("records"))
 
 fd = pd.read_excel(excel, 'Sheet1')
 date = date.today()
@@ -20,33 +61,17 @@ fd.insert(0, column="Sno", value=range(1, len(fd) + 1))
 print(fd.head())
 print(fd.to_dict("records"))
 
-print("1 ) If Want to print single value \n2 ) If want to print a range \n3 ) Print All")
-ch=input("Enter your choice")
+print("1 ) If want to print a range \n2 ) Print All")
+ch = input("Enter your choice")
 print(ch)
 
-if(ch=='1'):
-   sno=int(input("Enter the sno of the member"))   
-   for record in fd.to_dict("records"):       
-       if record['Sno'] == sno:
-           doc = DocxTemplate(word)
-           doc.render(record)
-           output_path = output / f"{record['Name']}-doc.docx"
-           doc.save(output_path)
-    print("Printed document with sno")
-    
-elif(ch=='2'):
-    range=input("Enter the Range seperated by - :ex 1-4 ")
-    lst=range.split('-')
-    print(lst)
-    a=int(lst[0])
-    b=int(lst[1])
-    for record in fd.to_dict("records"):
-        if a <= record['Sno'] <= b:
-            doc = DocxTemplate(word)
-            doc.render(record)
-            output_path = output / f"{record['Name']}-doc.docx"
-            doc.save(output_path)
-    print("Printed docs Succesfully")
+if ch == '1':
+    range = input("Enter the range")
+    if range.__contains__(","):
+        ele = range.split(",")
+        comma(ele)
+    elif range.__contains__("-"):
+        hifen(range)
 else:
     for record in fd.to_dict("records"):
         doc = DocxTemplate(word)
@@ -54,16 +79,37 @@ else:
         output_path = output / f"{record['Name']}-doc.docx"
         doc.save(output_path)
     print("Printed all the docs Successfully")
-        
 
-'''
-for record in fd.to_dict("records"):
-    if 2 <= record['Sno'] <= 4:
-        doc = DocxTemplate(word)
-        doc.render(record)
-        output_path = output / f"{record['Name']}-doc.docx"
-        doc.save(output_path)
-    '''
+# if (ch == '1'):
+#     sno = int(input("Enter the sno of the member"))
+#     for record in fd.to_dict("records"):
+#         if record['Sno'] == sno:
+#             doc = DocxTemplate(word)
+#             doc.render(record)
+#             output_path = output / f"{record['Name']}-doc.docx"
+#             doc.save(output_path)
+#     print("Printed document with sno")
+#
+# elif (ch == '2'):
+#     range = input("Enter the Range seperated by - :ex 1-4 ")
+#     lst = range.split('-')
+#     print(lst)
+#     a = int(lst[0])
+#     b = int(lst[1])
+#     for record in fd.to_dict("records"):
+#         if a <= record['Sno'] <= b:
+#             doc = DocxTemplate(word)
+#             doc.render(record)
+#             output_path = output / f"{record['Name']}-doc.docx"
+#             doc.save(output_path)
+#     print("Printed docs Succesfully")
+# else:
+#     for record in fd.to_dict("records"):
+#         doc = DocxTemplate(word)
+#         doc.render(record)
+#         output_path = output / f"{record['Name']}-doc.docx"
+#         doc.save(output_path)
+#     print("Printed all the docs Successfully")
 
 # if record['Done'].equals("True"):
 
@@ -77,4 +123,3 @@ for record in fd.to_dict("records"):
 #     count += 1
 #
 # pd.
-
